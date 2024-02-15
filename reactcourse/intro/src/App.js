73bf61875ec90c5f7aee35323fa04945e666d353
Tuba Türkoğlu <92tuba92@gmail.com>
2024-02-15 13:18:50 +0300

    categoryList, productList

--- reactcourse/intro/src/App.js
+++ reactcourse/intro/src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
-function App() {
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList title="Category List" />
-          </Col>
-          <Col xs="9">
-            <ProductList title="Product List"/>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+  state = { currentCategory: "" };
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+
+  render() {
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CategoryList" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
